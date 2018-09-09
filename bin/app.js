ddcd13a3a968e1a78d9eaf08f3be3419cc6606cf
PengChen96 <2601349541@qq.com>
2018-09-09 23:13:28 +0800

    add cfs

--- bin/app.js
+++ bin/app.js
@@ -6,39 +6,35 @@ const commander = require('./build/option.js');
 const config = require('../config');
 
 console.log('↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ 参数 ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓');
-console.log('commander:', commander);
-console.log('process.argv:', process.argv);
+//console.log('commander:', commander);
+//console.log('process.argv:', process.argv);
 console.log('PROJ:', config.PROJ);
+console.log('ROOT:', config.ROOT);
 console.log('↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ 参数 ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑');
-// commander.version('1.0.0')
-//   .option('-a, --api [des]', 'api mock cli')
-//   .option('-m, --mock [path]', 'mock api')
-//   .option('-p, --port [port]', 'set port')
 
-// commander
-//   .command('init <project>')
-//   .description('init project')
-//   .action((projectName) => {
-//     console.log(`init ${projectName}`);
-//   });
-
-// commander.parse(process.argv);
-
+let params = '';
 // $ ap-cli -a [des]
 if (commander.api) {
-  console.log('ap-cli:', commander.api);
+  params += ` --api ${commander.api}`;
+  console.log('api:', commander.api);
 }
 
 // $ ap-cli -m [path]
 if (commander.mock) {
-  console.log('ap-cli:', commander.mock);
+  params += ` --mock ${commander.mock}`;
+  console.log('mock:', commander.mock);
 }
 
 // $ ap-cli -p [port]
 if (commander.port) {
-  const argv = process.argv.splice(2, process.argv.length - 1);
-  console.log(`node ${config.PROJ} ${argv.join(' ')}`);
-  shell.exec(`node ${config.PROJ} ${argv.join(' ')}`);
-  // shell.exec('npm run dev1 -- --port 3001');
-  // shell.exec(`node ${config.PROJ} --port ${commander.port}`);
+  params += ` --port ${commander.port}`;
+  console.log('port:', commander.port);
 }
+
+
+// const argv = process.argv.splice(2, process.argv.length - 1);
+// console.log(`node ${config.ROOT}server ${argv.join(' ')}`);
+// finally exec
+console.log(`node ${config.ROOT}server${params}`);
+shell.exec(`node ${config.ROOT}server${params}`);
+
--- config.js
+++ config.js
@@ -1,6 +1,12 @@
 
 const PROJ = process.cwd();
 
+const path = require('path');
+const ROOT = path.join(__dirname, './');
+
 module.exports = {
-    PROJ
+  // 当前启动所在目录
+  PROJ,
+  // 项目的根目录
+  ROOT
 }
