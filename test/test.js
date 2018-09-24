bc940c8cdd328d85087e1f7de9b68fecc49cc61e
PengChen96 <2601349541@qq.com>
2018-09-24 12:38:30 +0800

    mocha test0.1.0

---
+++ test/test.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2018/9/24.
+ */
+
+const assert = require('assert');
+
+describe('#test.js', () => {
+  describe('#sum()', () => {
+    it('1+1 should return 2', () => {
+      assert.strictEqual(1 + 1, 2);
+    });
+  });
+});
