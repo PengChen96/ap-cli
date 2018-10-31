ed09a746e8e48ea2b4f802a5ccb2803ddcbb8b7d
cpg <2601349541@qq.com>
2018-10-31 20:36:07 +0800

    !!! server/index.js => server/router/index.js 将扫描文件添加路由移到router/index.js中

--- server/index.js
+++ server/index.js
@@ -1,22 +1,16 @@
 /* global require */
 
 const Koa = require('koa');
-const router = require('koa-router')();
 const cors = require('koa2-cors');
-const cfs = require('../common/cfs');
-const utils = require('../common/utils');
 const Logger = require('../common/logger');
-const parseSw = require('../common/parseSw');
-const config = require('../config');
 // commander options
 const options = require('../bin/build/option.js');
+// 扫描文件添加的路由
+const myRouter = require('./router/index');
 //
 const app = new Koa();
 // 跨域
 app.use(cors());
-// 参数
-const { ROOT, PROJ, FILE_WHITE_LIST } = config;
-// const { mock, port, regexp, swagger } = options;
 
 // 对于任何请求，app将调用该异步函数处理请求：
 app.use(async (ctx, next) => {
@@ -24,145 +18,8 @@ app.use(async (ctx, next) => {
   await next();
 });
 
-// add url-route:
-router.get('/', async (ctx, next) => {
-  ctx.response.body = `<h1>当前启动的文件${PROJ}${ options.mock || '' }</h1>`;
-});
 // add router middleware:
-app.use(router.routes());
+app.use(myRouter.routes());
 
-const listener = () => {
-  // 在端口3000监听:
-  app.listen(options.port || 3000);
-  Logger.SUCCESS(`app started at port ${options.port || 3000}...`);
-};
-
-/**
- * 扫描文件添加Api
- * @param options 命令行参数
- * 【mock, port, regexp】
- */
-const scanFileAddInterface = (options) => {
-  // 扫描当前目录下的文件,获取文件名数组
-  cfs.readdir(`${PROJ}`).then((res) => {
-    // 执行当前目录下的文件【js, json】
-    if (options.mock === 'true' || options.mock === 'all' ) {
-      // 获取白名单内的文件数组
-      const result = res.filter((fileName) => {
-        let flag = false;
-        const suffix = utils.getFileNameSuffix(fileName);
-        // 文件后缀白名单
-        if (FILE_WHITE_LIST.includes(suffix)) {
-          flag = true;
-        } else {
-          Logger.WARN(`${fileName}不在白名单内！\n可通过修改${ROOT}下的config.js添加白名单！`);
-        }
-        return flag;
-      });
-      let filterResult = result;
-      // 正则匹配文件
-      if (options.regexp) {
-        // 转换为正则
-        const regexp = utils.strToRegExp(options.regexp);
-        filterResult = result.filter((item) => regexp.test(item));
-      }
-      Logger.INFO(`扫描到 ${filterResult} 这${filterResult.length}个文件`);
-      filterResult.forEach((fileName) => {
-        // 异步读取文件内容
-        initRouter(fileName);
-      });
-    } else { // 执行该目录下指定的文件
-      // 异步读取文件内容
-      initRouter(options.mock);
-    }
-  }).catch((error) => {
-    Logger.ERROR(error);
-  });
-};
-// swagger解析 TODO
-const parseSwagger = (fileName) => {
-  cfs.readFile(`${PROJ}/${fileName}`).then((result) => {
-    const resp = JSON.parse(result);
-    // 要写入的新文件
-    const filePath = `${PROJ}\\${fileName.split('.')[0]}_new.json`;
-    // 要写入的内容
-    const content = parseSw.formatSwaggerData(resp);
-    cfs.writeFile(filePath, JSON.stringify(content)).then((data) => {
-      if (data) {
-        Logger.SUCCESS(`生成${filePath}成功！`);
-      }
-    }).catch((error) => {
-      Logger.ERROR(`writeFile -- ${error}`);
-    });
-  }).catch((error) => {
-    Logger.ERROR(error);
-  });
-};
-/**
- * 启动服务
- */
-const startRun = () => {
-  if (options.swagger) {
-    // 解析swagger文件
-    parseSwagger(options.swagger);
-  }
-  if (options.mock) {
-    // 启动 扫描文件添加Api
-    scanFileAddInterface(options);
-    // 监听端口（默认3000）
-    listener();
-  } else {
-    Logger.WARN('-m [fileName]为启动mock服务的必须参数;\n[fileName]为当前目录下的文件名，默认all当前目录下的所有文件;')
-  }
-};
-startRun();
-
-
-/**
- * 初始化路由（接口） 【js, json】
- * @param fileName 文件名  example: exam.js，exam.json
- */
-const initRouter = (fileName) => {
-  const suffix = utils.getFileNameSuffix(fileName);
-  if (suffix === 'js') {
-    requireJsInitRouter(fileName);
-  } else {
-    readFileInitRouter(fileName);
-  }
-};
-/**
- * 通过读取文件内容 初始化路由
- * @param fileName 文件名  example: exam.json
- */
-const readFileInitRouter = (fileName) => {
-  cfs.readFile(`${PROJ}/${fileName}`).then((result) => {
-    const resp = JSON.parse(result);
-    Logger.SUCCESS(`[${fileName}] 添加 ${resp.length} 个接口`);
-    resp.forEach((item) => {
-      // router
-      Logger.TRACE(`[${fileName}] init router 【${item.URI}】`);
-      router[item.method](item.URI, async (ctx, next) => {
-        ctx.response.body = item.response;
-      });
-    });
-  }).catch((error) => {
-    Logger.ERROR(`${fileName}--readFileInitRouter--${error}`);
-  });
-};
-/**
- * 通过引入JS 初始化路由
- * @param fileName 文件名  example: exam.js
- */
-const requireJsInitRouter = (fileName) => {
-  // 引入js文件
-  const FunObj = require(`${PROJ}/${fileName}`);
-  Logger.SUCCESS(`[${fileName}] 添加 ${Object.keys(FunObj).length} 个接口`);
-  for (const funName in FunObj) {
-    const item = FunObj[funName](); // 接口数据
-    Logger.TRACE(`[${fileName}] init router 【${item.URI}】`);
-    router[item.method](item.URI, async (ctx, next) => {
-      ctx.response.body = item.response;
-    });
-  }
-};
-
+app.listen(options.port || 3000);
+Logger.SUCCESS(`app started at port ${options.port || 3000}...`);
