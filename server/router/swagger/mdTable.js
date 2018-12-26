3a80f05ea4e28eee9a49f68d9a9b1c0f2eb0125e
cpg <2601349541@qq.com>
2018-12-26 08:57:12 +0800

    mdTable style

--- server/router/swagger/mdTable.js
+++ server/router/swagger/mdTable.js
@@ -6,7 +6,12 @@
  * @param ref   example: "#/definitions/apiVO"
  * @param swaggerData
  */
-const dealRef = (ref, swaggerData) => {
+const dealRef = (ref, swaggerData, padding = 2) => {
+  let padd = padding;
+  let space = '';
+  for (let i=0; i<padd; i++) {
+    space += '&emsp;';
+  }
   let result = [];
   let definitionsVo = queryDataVo(ref, swaggerData);
   let type = definitionsVo && definitionsVo.type ? definitionsVo.type : '';
@@ -16,7 +21,7 @@ const dealRef = (ref, swaggerData) => {
       if (properties) {
         for (let key in properties) {
           if (properties[key].type) {
-            const rowArr = convertRowArr(ref, properties, key, swaggerData);
+            const rowArr = convertRowArr(ref, properties, key, swaggerData, padd);
             result = [...result, ...rowArr];
           }
           /**
@@ -24,8 +29,11 @@ const dealRef = (ref, swaggerData) => {
            * properties[key]['$ref'] !== ref，判断一下这个【$refs】和上一个【$refs】是否一样，避免死循环
            */
           if (properties[key]['$ref'] && properties[key]['$ref'] !== ref) {
-            const rowArr = dealRef(properties[key]['$ref'], swaggerData);
-            result = [...result, ...rowArr];
+            const { type='object', required='false', description='说明' } = properties[key];
+            const faRowArr = [`|${space}${key}|${type}|${required}|${description}|\n`];
+            let pd = padding + 2;
+            const rowArr = dealRef(properties[key]['$ref'], swaggerData, pd);
+            result = [...result, ...faRowArr, ...rowArr];
           }
         }
       }
@@ -52,15 +60,16 @@ const queryDataVo = (ref, swaggerData) => {
 /**
  *  转换为md数组（一维数组）
  */
-const convertRowArr = (ref, properties, key, swaggerData) => {
+const convertRowArr = (ref, properties, key, swaggerData, padding) => {
   let RowArr = [];
-  let space = '&emsp;&emsp;';
+  // 空格
+  let space = convertToSpace(padding);
   const { type='object', required='false', description='说明', items } = properties[key];
   if (type == 'array') {
     // items['$ref']!==ref，判断一下这个【$refs】和上一个【$refs】是否一样，避免死循环
     if (items && items['$ref'] && items['$ref']!==ref) {
       const faRowArr = [`|${space}${key}|${type}|${required}|${description}|\n`];
-      const mdRowArr = dealRef(items['$ref'], swaggerData);
+      const mdRowArr = dealRef(items['$ref'], swaggerData, padding + 2);
       RowArr = [...faRowArr, ...mdRowArr]; 
     } else {
       // 这个情况考虑下 "items": {"type": "object"}
@@ -69,9 +78,24 @@ const convertRowArr = (ref, properties, key, swaggerData) => {
   } 
   else if (type === 'object') {}
   else {
-    RowArr = [`|${space}${key}|${type}|${required}|${description}|\n`];
+    if (items && items['$ref'] && items['$ref']!==ref) {
+      const faRowArr = [`|${space}${key}|${type}|${required}|${description}|\n`];
+      const mdRowArr = dealRef(items['$ref'], swaggerData, padding + 2);
+      RowArr = [...faRowArr, ...mdRowArr]; 
+    } else {
+      RowArr = [`|${space}${key}|${type}|${required}|${description}|\n`];
+    }
   }
   return RowArr;
+};
+
+// 转换成‘&emsp’空格
+const convertToSpace = (padding) => {
+  let space = '';
+  for (let i = 0; i < padding; i++) {
+    space += '&emsp;';
+  }
+  return space;
 };
 
 module.exports = {
